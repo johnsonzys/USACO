0e0077484622373d30543e994866654927e8caaf
johnsonzys <johnsonz.ysimail@gmail.com>
2022-12-13 19:55:58 -0800

    ride

--- Training/1-2-2/ride.java
+++ Training/1-2-2/ride.java
@@ -1,7 +1,7 @@
 /* Use the slash-star style comments or the system won't see your
    identification information */
 /*
-ID: your_id_here
+ID: johnson40
 LANG: JAVA
 TASK: ride
 */
@@ -10,38 +10,28 @@ import java.util.*;
 
 class ride {
    public static void main (String[] args) throws IOException{
-      Scanner in = new Scanner(new File("ride.in"));
-      PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
-      // String s1,s2 = inputs.split('\n');
+      BufferedReader f = new BufferedReader(new FileReader("ride.in"));
+      PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
 
-      // while(in.hasNextLine()){
-      String s1 = in.nextLine();
-      String s2 = in.nextLine();
-      // }
+      String comet = f.readLine();
+      String ppl = f.readLine();
 
-      char c1 = ' ';
-      char c2 = ' ';
-      int prod_sum1 = 1; // use 1 to avoid 0x0
-      int prod_sum2 = 1; // use 1 to avoid 0x0
+      int sum_c = 1;
+      int sum_p = 1;
 
-      for(int i=0;i<s1.length();i++){
-         c1 = s1.charAt(i);
-         prod_sum1 *= (int)c1-64;
+      for(int i=0;i<comet.length();i++){ //iterate every element in String comet
+         char c = comet.charAt(i);
+         sum_c *= (int)c - 64; //C -> 3
       }
 
-      for(int i=0;i<s2.length();i++){
-         c2 = s2.charAt(i);
-         prod_sum2 *= (int)c2-64;
+      for(int i=0;i<ppl.length();i++){ //iterate every element in String comet
+         char c = ppl.charAt(i);
+         sum_p *= (int)c - 64; //C -> 3
       }
 
-      if(prod_sum1%47==prod_sum2%47){
-         out.println("GO");
-      }
-      else{
-         out.println("STAY");
-      }
-
-      
-      out.close();                                  // close the output file
+      int diff = sum_c - sum_p;
+      if(diff%47==0){pw.println("GO");}
+      else{pw.println("STAY");}
+      pw.close();
    }
 }
