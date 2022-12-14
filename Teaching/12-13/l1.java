99493bc0944e30c27579fc4963045f6ef4a297a2
johnsonzys <johnsonz.ysimail@gmail.com>
2022-12-13 19:18:47 -0800

    l1 complete

--- Teaching/12-13/l1.java
+++ Teaching/12-13/l1.java
@@ -7,12 +7,17 @@ public class l1 {
         String s = f.readLine();
         String s1 = f.readLine();
 
-        Scanner in = new Scanner(new File("l1.in"));
-        String n = in.nextLine();
+        // Scanner in = new Scanner(new File("l1.in"));
+        // String n = in.nextLine();
 
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("l1.out")));
 
-        out.println(s1+s+n);
+        System.out.println("Yes"); //print to terminal
+        out.print(s1+"\n"+s); // print to l1.out
+        // out.println();
+        int i = 1;
+        String ii = ""+i;
+
         out.close();
     }
 }
