10f2f7e479731daef8f51e67a573b9cc61a44707
Johnson Z <111026348+johnsonzys@users.noreply.github.com>
2022-12-15 04:09:48 +0000

    12-14

--- Teaching/12-14/l2.java
+++ Teaching/12-14/l2.java
@@ -2,8 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class l2 {
-    public static void main(String [] args) throws IOException{
-        BufferedReader br = new BufferedReader(new FileReader("l2.in"));
-        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("l2.out")));
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new FileReader("l2.in"));
+    PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("l2.out")));
+
+    int n = Integer.parseInt(br.readLine()); // number of cows
+    char[] cows = br.readLine().toCharArray(); // string
+  
+    int result = 0;
+  
+    for(int i = cows.length - 2;i>0;i-=2)
+    {
+      if (cows[i] != cows[i + 1] && (cows[i] == 'G') == (result % 2 == 0)) {
+        result++;
+      }
     }
+    pw.println(result);
+    pw.close();
+   }
 }
