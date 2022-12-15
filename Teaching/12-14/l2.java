2dddb7d597aa988ec3f33036b305e171045654af
johnsonzys <johnsonz.ysimail@gmail.com>
2022-12-14 17:49:44 -0800

    12-14

---
+++ Teaching/12-14/l2.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class l2 {
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("l2.in"));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("l2.out")));
+    }
+}
